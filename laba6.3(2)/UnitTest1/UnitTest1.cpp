cc6e6d5ddf87d811cb750336215180c0e2bf757c
Sofia22012005 <114824939+Sofia22012005@users.noreply.github.com>
2022-12-01 01:11:02 +0200

    Add files via upload

---
+++ laba6.3(2)/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../laba6.3(2)/laba6.3(2).cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+namespace UnitTest3
+{
+	TEST_CLASS(UnitTest3)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			int a[] = { 23, 21, 6, 12, -3 };
+
+			int res = Min(a, 5, 0, 0);
+			Assert::AreEqual(res, -3);
+		}
+	};
+}
+
++
